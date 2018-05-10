e8756ab6c84cc1c9fdb8368cfb7beb9424d9b430
Omar <omar@sititec.com>
2018-05-10 12:45:19 -0500

    deleting files

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-echo "Hello my name is Heroku app :D";
-echo '<br>New line<br>';
-
-
-echo "==>" . getenv('NAME');
--- vendor/midgard/midgardmvc-helper-urlize/tests/travis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install dependencies with Composer
-wget http://getcomposer.org/composer.phar 
-php composer.phar install
