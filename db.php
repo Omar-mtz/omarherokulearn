3136fad0f08adacb25e8259940856660aacfe7c6
Omar <omar@sititec.com>
2018-05-10 11:38:01 -0500

    change db info

--- db.php
+++ db.php
@@ -1,10 +1,14 @@
 <?php
 
-$db = mysqli_connect("localhost", "root", "root", "morena_db");
+$db = mysqli_connect("us-cdbr-iron-east-04.cleardb.net/heroku_58696cbe5477847", "b1129d569e3212", "c6278534");
  
 if($db === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
  
 echo "Connect Successfully. Host info: " . mysqli_get_host_info($db);
-?>
+
+
+//mysql://b1129d569e3212:c6278534@us-cdbr-iron-east-04.cleardb.net/heroku_58696cbe5477847?reconnect=true
+
+
